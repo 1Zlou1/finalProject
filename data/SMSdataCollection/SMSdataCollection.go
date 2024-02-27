0400e4af921a2f78b9a46d4b0155f5c30d0fae03
Stepan_Furmanov <stepan.furmanov@gmail.com>
2024-02-27 19:41:50 +0300

    .

--- data/SMSdataCollection/SMSdataCollection.go
+++ data/SMSdataCollection/SMSdataCollection.go
@@ -7,10 +7,10 @@ import (
 )
 
 type SMSData struct {
-	Country      string
-	Bandwidth    string
-	ResponseTime string
-	Provider     string
+	Country      string `json:"country"`
+	Bandwidth    string `json:"bandwidth"`
+	ResponseTime string `json:"response_time"`
+	Provider     string `json:"provider"`
 }
 
 func readAndParseFile() []SMSData {
--- service/service.go
+++ service/service.go
@@ -1,1 +1,27 @@
-package service
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleConnection).Methods("GET")
+
+	srv := &http.Server{
+		Addr:    "localhost:8282",
+		Handler: r,
+	}
+
+	fmt.Println("Сервер запущен на localhost:8282")
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Printf("Ошибка запуска сервера: %v\n", err)
+	}
+}
